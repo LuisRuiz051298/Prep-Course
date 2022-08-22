77484b100afc79013f1b3f0fd49beda7c6c0791e
kevindryp <mateodryp@gmail.com>
2022-08-22 17:44:40 -0500

    cuarto comentario

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -4,7 +4,7 @@
 const nuevaString = 'hola como estas';
 
 // Crea una variable numérica, puede ser cualquier número:
-const nuevoNum = 790 ;
+const nuevoNum = 805 ;
 
 // Crea una variable booleana:
 const nuevoBool = null;
