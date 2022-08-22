5962cf5848698d3b3cd1a40ea99a7fef6bdcafc1
kevindryp <mateodryp@gmail.com>
2022-08-22 17:48:49 -0500

    quinto comentario

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -1,10 +1,11 @@
 // En estas primeras 6 preguntas, reemplaza `null` por la respuesta
 
 // Crea una variable "string", puede contener lo que quieras:
-const nuevaString = 'hola como estas';
+const nuevaString = 'Espiritu santo te amamos';
 
 // Crea una variable numérica, puede ser cualquier número:
-const nuevoNum = 805 ;
+const nuevoNum = 805
+ ;
 
 // Crea una variable booleana:
 const nuevoBool = null;
